1808079a782b86883b595840b0f26cc0efecea5d
Frang998 <202301209@daiict.ac.in>
2024-06-29 19:28:55 +0530

    Start Updating code

---
+++ One_esp_updated/updated_main.cpp
@@ -0,0 +1,17 @@
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <Adafruit_BMP280.h>
+#include <Wire.h>
+#include <WiFi.h>
+#include <ModbusRTU.h>
+#include <ArduinoJson.h>
+#include <PubSubClient.h>
+
+const char* ssid="SSID";
+const char* password="Password";
+
+String deviceId="device0001";
+const char* mqttServer="192.168.1.100";
+const int mqttPort=1883;
+const char* mqttUser="device0001";
