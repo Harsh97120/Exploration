b487a78db60a7b36a334d17e0eea76644b9b56f8
Harsh97120 <202301192@daiict.ac.in>
2024-09-05 21:48:24 +0530

    Finalize slave master for one value exchange

--- ModbusRTU/commu_between_two/master.cpp
+++ ModbusRTU/commu_between_two/master.cpp
@@ -4,6 +4,7 @@
 #define TX 12
 
 #define SLAVE_ID 1
+#define HREG 1
 
 bool readTask = false;
 
@@ -16,17 +17,13 @@ void setup()
     Serial.begin(9600, SERIAL_8N1);
     Serial2.begin(9600, SERIAL_8N1, RX, TX);
     mb.begin(&Serial2); // Initializes Modbus on Serial2
-    mb.slave(1);
-
-    // Set up holding register
-    mb.addHreg(1, 0);
+    mb.master();
 }
 
 void loop()
 {
-    mb.task();
-    // mb.readIreg(SLAVE_ID, 1, value, 1, cbRead);
 
+    // mb.readIreg(SLAVE_ID, 1, value, 1, cbRead);
     // if(readTask)
     // {
     //     Serial.print("Value is Received.\n");
@@ -34,10 +31,16 @@ void loop()
     //     Serial.print(value);
     // }
 
-    read_value = mb.Hreg(HREG); // Reads the value of HREG and stores it in read_value
-    Serial.print("Reading the value of holding register : ");
-    Serial.println(read_value);
+    // read_value = mb.Hreg(HREG); // Reads the value of HREG and stores it in read_value
+    // Serial.print("Reading the value of holding register : ");
+    // Serial.println(read_value);
 
+    if (!mb.readHreg(SLAVE_ID, HREG, &value, 1, cb))
+    {
+        Serial.println("Modbus request failed");
+    }
+
+    mb.task();
     delay(1000);
 }
 
@@ -55,4 +58,16 @@ bool cbRead(Modbus::ResultCode event, uint16_t transactionId, void *data)
         Serial.println(event);
         return false;
     }
+}
+
+bool cb(Modbus::ResultCode event, uint16_t transactionId, void *data)
+{
+    Serial.print("Request result: 0x");
+    Serial.println(event, HEX);
+    if (event == Modbus::EX_SUCCESS)
+    {
+        Serial.print("Received value: ");
+        Serial.println(value);
+    }
+    return true;
 }
--- ModbusRTU/commu_between_two/slave.cpp
+++ ModbusRTU/commu_between_two/slave.cpp
@@ -7,26 +7,23 @@
 
 ModbusRTU mb;
 
+uint16_t write_value = 100;
+
 void setup()
 {
     Serial.begin(9600);
     Serial2.begin(9600, SERIAL_8N1, RX, TX);
     mb.begin(&Serial2); // Initializing Modbus on Serial2 
-    mb.master();
+    mb.slave(SLAVE_ID);
+    mb.addHreg(HREG,write_value);
 }
 
-uint16_t write_value = 100;
+
 
 void loop()
 {
-    mb.task(); // The magic function which handles all the communication
+    mb.task(); 
+    mb.addHreg(HREG,write_value);
 
-    if (!mb.slave())
-    {
-        mb.writeHreg(SLAVE_ID, HREG, write_value, nullptr);
-        Serial.print("Writing the value : ");
-        Serial.println(write_value);
-    }
-
-    delay(100);
+    delay(1000);
 }
